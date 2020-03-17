efb68663f8215ac5b6381d9cd0f0197a8ce596e3
madhouny <younes.madhoun@gmail.com>
2020-03-17 20:13:02 +0100

    Creating the article view including a form (adding comment) and file css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,8 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
           integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{URL::to('src/Css/main.css')}}">
 </head>
 <body>
 @include('includes.header')
--- resources/views/post_article.blade.php
+++ resources/views/post_article.blade.php
@@ -2,14 +2,62 @@
 
 @section('content')
 
-    <section>
+    <section class="row new-post">
+    <div class="col-md-6 col-md-offset-3 ">
+        <header> <h2>Content of article </h2> </header>
         <p>{{$article->post_content}}</p>
 
-        <div class="info">
-            Posted by {{$article->post_name}} on {{$article->created_at}}
+        <div class="crud">
+            <a href="">Edit</a>
+            <a href="">Delete</a>
         </div>
 
+        <form action="">
+            <div class="form-group">
+               <textarea rows="5" aria-label="With textarea" cols="90" name="new-post" id="new-post"  placeholder="Add Comment"></textarea>
+
+            </div>
+
+            <button type="submit" class="btn-primary">Add Comment</button>
+        </form>
+    </div>
     </section>
+
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say .. </h3></header>
+            <article class="post">
+                <p></p>
+
+                <div class="info">
+                    Posted by
+                </div>
+
+                <div class="interaction">
+                    <a href="">Like</a>
+                    <a href="">DisLike</a>
+                    <a href="">Edit</a>
+                    <a href="">Delete</a>
+                </div>
+            </article>
+
+            <article class="post">
+                <p></p>
+
+                <div class="info">
+                    Posted by
+                </div>
+
+                <div class="interaction">
+                    <a href="">Like</a>
+                    <a href="">DisLike</a>
+                    <a href="">Edit</a>
+                    <a href="">Delete</a>
+                </div>
+            </article>
+        </div>
+    </section>
+
 
 
 @endsection
